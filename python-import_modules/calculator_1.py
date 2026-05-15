27093ec00e5991db2318b20770f07f63bd6324f7
Joshua SI <joshuasantiago111@gmail.com>
2026-05-15 18:52:44 -0400

    1-calculation handle add/sub/mult/div

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Calculator functions: add, sub, mul, div"""
+
+
+def add(a, b):
+    """Return the addition of a and b"""
+    return a + b
+
+
+def sub(a, b):
+    """Return the subtraction of a and b"""
+    return a - b
+
+
+def mul(a, b):
+    """Return the multiplication of a and b"""
+    return a * b
+
+
+def div(a, b):
+    """Return the integer division of a by b"""
+    return int(a / b)
