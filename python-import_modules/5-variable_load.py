42dcefd399b7f4f1fbbcc5e598665c09e3093e03
Joshua SI <joshuasantiago111@gmail.com>
2026-05-17 02:31:41 -0500

    python-var_loading

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,9 +1,12 @@
-# /path/to/your/python-import_modules/5-variable_load.py
+#!/usr/bin/python3
 
 def import_variable(module_name, var_name):
     module = __import__(module_name)
     return getattr(module, var_name)
 
 if __name__ == "__main__":
-    a = import_variable("variable_load_5", "a")
-    print(a)
+    try:
+        a = import_variable("variable_load_5", "a")
+        print(a)
+    except AttributeError:
+        print("Variable 'a' is not defined in the module.")
