172ec7019f414c97b59bb9c9f4ad1a5158f01d08
Joshua SI <joshuasantiago111@gmail.com>
2026-05-23 12:11:47 -0500

    Fix variable import script

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 
-def import_variable(module_name, var_name):
-    module = __import__(module_name)
-    return getattr(module, var_name)
+import variable_load_5
+
 
 if __name__ == "__main__":
-    try:
-        a = import_variable("variable_load_5", "a")
-        print(a)
-    except ImportError:
-        print("Module 'variable_load_5' is not found.")
-    except AttributeError:
-        print("Variable 'a' is not defined in the module.")
+    print(variable_load_5.a)
         
