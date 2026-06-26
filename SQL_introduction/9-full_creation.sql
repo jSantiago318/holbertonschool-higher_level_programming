9d0bb10f2b03d196e524efad53d95ea6d3382476
Joshua SI <joshuasantiago111@gmail.com>
2026-06-26 19:36:34 -0400

    Add SQL_introduction task 9: full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Creates second_table if missing and inserts the initial records
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table (id, name, score) VALUES
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
