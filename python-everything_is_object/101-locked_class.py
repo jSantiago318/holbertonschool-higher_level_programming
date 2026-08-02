18eba8a0858352cf1e1479b6c37dcb732a99022b
Joshua SI <joshuasantiago111@gmail.com>
2026-08-02 14:24:23 -0400

    Document 101-locked_class module and class

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""Defines a class that locks down dynamic instance attributes."""
+
+
 class LockedClass:
+    """Only allows the instance attribute ``first_name`` to be created."""
+
     __slots__ = ("first_name",)
